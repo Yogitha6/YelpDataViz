c8e5bf279f2c51430aa515cab3e10ee186c1319c
Yogitha6 <yoma2088@colorado.edu>
2016-11-25 00:58:15 -0700

    linked parallel coordinates and leaflet map

--- YelpDataViz/YelpRestaurantsViz/views.py
+++ YelpDataViz/YelpRestaurantsViz/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     existingCities = ['Phoenix','Karlsruhe','Edinburgh','Urbana-Champaign','Charlotte','LasVegas','Waterloo','Pittsburgh','Montreal','Madison']
-    return render(request, "canvasParallelCoordinates.html", {'cities': existingCities, 'selectedCity': 'Charlotte'})
+    return render(request, "canvasParallelCoordinates - Copy.html", {'cities': existingCities, 'selectedCityName': 'Charlotte', 'selectedCityLatitude': 35.22, 'selectedCityLongitude': -80.84})
 
 def selectCity(request, cityname):
     existingCities = ['Phoenix','Karlsruhe','Edinburgh','Urbana-Champaign','Charlotte','LasVegas','Waterloo','Pittsburgh','Montreal','Madison']
-    return render(request, "canvasParallelCoordinates.html", {'cities': existingCities, 'selectedCity': cityname})
+    latitudeDict = {'Phoenix': 33.44,'Karlsruhe': 49.006,'Edinburgh': 55.95,'Urbana-Champaign': 40.110 ,'Charlotte': 35.22,'LasVegas': 36.169,'Waterloo': 43.46,'Pittsburgh': 40.44,'Montreal': 45.50,'Madison': 43.07}
+    longitudeDict = {'Phoenix': -112.07,'Karlsruhe': 8.403,'Edinburgh': -3.18,'Urbana-Champaign': -88.207 ,'Charlotte': -80.84,'LasVegas': 115.13,'Waterloo': -80.52,'Pittsburgh': -79.99,'Montreal': -73.56 ,'Madison': -89.40}
+    return render(request, "canvasParallelCoordinates - Copy.html", {'cities': existingCities, 'selectedCityName': cityname, 'selectedCityLatitude': latitudeDict[cityname], 'selectedCityLongitude': longitudeDict[cityname]})
